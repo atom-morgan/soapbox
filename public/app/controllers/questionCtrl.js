6b533b97bd63c99c13ba97bb2aacec1ea6c0e3bf
Adam Morgan <atom.morgan@gmail.com>
2015-05-14 21:01:33 -0400

    Prevent users from voting on the same question multiple times

--- public/app/controllers/questionCtrl.js
+++ public/app/controllers/questionCtrl.js
@@ -30,29 +30,35 @@ angular.module('questionCtrl', [])
     });
 
   vm.upvote = function(question) {
-    vm.voteData.voter = vm.currentUser.username;
-    vm.voteData.upvote = true;
-    vm.voteData.downvote = false;
+    var userHasVoted = hasUserVoted(question.voters, "upvote");
+    if (!userHasVoted) {
+      vm.voteData.voter = vm.currentUser.username;
+      vm.voteData.upvote = true;
+      vm.voteData.downvote = false;
 
-    Question.vote(question._id, vm.voteData)
-      .success(function(data) {
-        console.log(data.message);
-        var new_vote = initUpdatedVote(question._id, "upvote");
-        $rootScope.$broadcast('vote-updated', new_vote);
-      });
+      Question.vote(question._id, vm.voteData)
+        .success(function(data) {
+          console.log(data.message);
+          var new_vote = initUpdatedVote(question._id, "upvote");
+          $rootScope.$broadcast('vote-updated', new_vote);
+        });
+    }
   };
 
   vm.downvote = function(question) {
-    vm.voteData.voter = vm.currentUser.username;
-    vm.voteData.upvote = false;
-    vm.voteData.downvote = true;
+    var userHasVoted = hasUserVoted(question.voters, "downvote");
+    if (!userHasVoted) {
+      vm.voteData.voter = vm.currentUser.username;
+      vm.voteData.upvote = false;
+      vm.voteData.downvote = true;
 
-    Question.vote(question._id, vm.voteData)
-      .success(function(data) {
-        console.log(data.message);
-        var new_vote = initUpdatedVote(question._id, "downvote");
-        $rootScope.$broadcast('vote-updated', new_vote);
-      });
+      Question.vote(question._id, vm.voteData)
+        .success(function(data) {
+          console.log(data.message);
+          var new_vote = initUpdatedVote(question._id, "downvote");
+          $rootScope.$broadcast('vote-updated', new_vote);
+        });
+    }
   };
 
   function initUpdatedVote(question_id, vote) {
@@ -60,5 +66,13 @@ angular.module('questionCtrl', [])
     new_vote.id = question_id;
     new_vote.vote = vote;
     return new_vote;
+  }
+
+  function hasUserVoted(votes, voteType) {
+    var userVoteIndex = votes.map(function(vote) {
+      return vote.voter;
+    }).lastIndexOf(vm.currentUser.username);
+
+    if (userVoteIndex !== -1) { return votes[userVoteIndex][voteType]; }
   }
 });
